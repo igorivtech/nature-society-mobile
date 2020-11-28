8b476442efcdb518ba243bdb47ec027db15cd15c
Nimrod <nimrodbens@gmail.com>
2020-11-28 17:15:00 +0200

    deleted space

--- App.js
+++ App.js
@@ -26,7 +26,7 @@ export default function App() {
 
   return (
     <NavigationContainer>
-      <HomeStack.Navigator initialRouteName='Onboarding' headerMode='none' >
+      <HomeStack.Navigator initialRouteName='Onboarding' headerMode='none'>
         <HomeStack.Screen name="Onboarding" component={OnboardingScreen} />
         <HomeStack.Screen name="Home" component={HomeScreen} />
       </HomeStack.Navigator>
