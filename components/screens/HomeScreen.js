567004702aded506b7a0a408e5d012178f54db7b
Nimrod <nimrodbens@gmail.com>
2020-11-29 13:32:07 +0200

    working on main flat list style

--- components/screens/HomeScreen.js
+++ components/screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import {
   View,
   Text,
@@ -16,9 +16,17 @@ import { height, width } from "../../values/consts";
 const SPACING = 40;
 const CARD_TRANSLATE_Y = 20;
 const ITEM_WIDTH = width - 2 * SPACING;
+// const ITEM_WIDTH = width * 0.79;
 const ITEM_HEIGHT = ITEM_WIDTH * 0.466;
+const SPACER_ITEM_SIZE = (width - ITEM_WIDTH) / 2;
 
 export const HomeScreen = () => {
+  const [places, setPlaces] = useState([]);
+
+  useEffect(() => {
+    setPlaces([{ key: "left-spacer" }, ...DATA, { key: "right-spacer" }]);
+  }, []);
+
   const scrollX = useRef(new Animated.Value(0)).current;
 
   const progress = () => {
@@ -45,15 +53,15 @@ export const HomeScreen = () => {
       </SafeAreaView>
 
       <Animated.FlatList
-        data={DATA}
+        data={places}
         horizontal
         style={{
           paddingTop: CARD_TRANSLATE_Y,
           // backgroundColor: 'yellow',
           right: 0,
           left: 0,
-          position: 'absolute',
-          bottom: 0
+          position: "absolute",
+          bottom: 0,
         }}
         contentContainerStyle={{
           alignItems: "center",
@@ -63,14 +71,18 @@ export const HomeScreen = () => {
           { useNativeDriver: true }
         )}
         scrollEventThrottle={16}
+        onMomentumScrollEnd={(e)=>console.log(e.nativeEvent)}
         keyExtractor={(item) => item.key}
         snapToInterval={ITEM_WIDTH}
         decelerationRate={0}
         bounces={false}
         showsHorizontalScrollIndicator={false}
-        renderItem={({ item, index }) => (
-          <PlaceCard index={index} item={item} scrollX={scrollX} />
-        )}
+        renderItem={({ item, index }) => {
+          if (index == 0 || index == places.length - 1) {
+            return <View style={{width: SPACER_ITEM_SIZE}} />
+          }
+          return <PlaceCard index={index} item={item} scrollX={scrollX} />;
+        }}
       />
     </View>
   );
@@ -78,9 +90,9 @@ export const HomeScreen = () => {
 
 const PlaceCard = ({ item, index, scrollX }) => {
   const inputRange = [
+    (index - 2) * ITEM_WIDTH,
     (index - 1) * ITEM_WIDTH,
-    index * ITEM_WIDTH,
-    (index + 1) * ITEM_WIDTH,
+    (index) * ITEM_WIDTH,
   ];
 
   const translateY = scrollX.interpolate({
@@ -100,7 +112,7 @@ const PlaceCard = ({ item, index, scrollX }) => {
           margin: 8,
           flex: 1,
           alignItems: "center",
-          justifyContent: 'space-around',
+          justifyContent: "space-around",
           backgroundColor: "red",
           transform: [{ translateY }],
         }}
