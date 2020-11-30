6c8e5d525f17d3dc120a3a2eae49e9c2abc7a97b
Nimrod <nimrodbens@gmail.com>
2020-11-30 11:00:02 +0200

    added swipe down

--- components/screens/PlaceScreen.js
+++ components/screens/PlaceScreen.js
@@ -13,12 +13,20 @@ import { strings } from "../../values/strings";
 import { styles } from "../../values/styles";
 import { textStyles } from "../../values/textStyles";
 
-import * as Animatable from 'react-native-animatable';
+import * as Animatable from "react-native-animatable";
 import { RecentVisitor } from "../views/home/views";
-import { height, NAV_CLOSE_TAP_SIZE, recentVisitors } from "../../values/consts";
+import {
+  height,
+  NAV_CLOSE_TAP_SIZE,
+  recentVisitors,
+} from "../../values/consts";
+import {
+  Directions,
+  FlingGestureHandler,
+  State,
+} from "react-native-gesture-handler";
 
 const fadeOutDuration = 100;
-
 
 export const PlaceScreen = ({ navigation, route }) => {
   const { place, locked } = route.params;
@@ -31,15 +39,15 @@ export const PlaceScreen = ({ navigation, route }) => {
 
   const waze = () => {
     console.log("waze");
-  }
+  };
 
   const share = () => {
     console.log("share");
-  }
+  };
 
   const report = () => {
     console.log("report");
-  }
+  };
 
   const goBack = () => {
     Promise.all([
@@ -47,99 +55,166 @@ export const PlaceScreen = ({ navigation, route }) => {
       ratingRef.current.fadeOut(fadeOutDuration),
       visitorsRef.current.fadeOut(fadeOutDuration),
       descRef.current.fadeOut(fadeOutDuration),
-      actionsRef.current.fadeOut(fadeOutDuration)
-    ]).then(() => navigation.goBack())
+      actionsRef.current.fadeOut(fadeOutDuration),
+    ]).then(() => navigation.goBack());
     // textRef.current.fadeOut(fadeOutDuration);
     // ratingRef.current.fadeOut(fadeOutDuration);
     // visitorsRef.current.fadeOut(fadeOutDuration);
     // descRef.current.fadeOut(fadeOutDuration);
     // actionsRef.current.fadeOut(fadeOutDuration);
     // navigation.goBack();
-  }
+  };
+
+  const handleSwipeDown = (event) => {
+    if (event.nativeEvent.state === State.END) {
+      goBack();
+    }
+  };
 
   return (
     <View style={s.container}>
-      <TouchableWithoutFeedback
-        onPress={goBack}
-        style={s.tap}
-      >
+      <TouchableWithoutFeedback onPress={goBack} style={s.tap}>
         <View style={StyleSheet.absoluteFill} />
       </TouchableWithoutFeedback>
 
-      <View style={s.containerStyle}>
-        <SharedElement
-          style={StyleSheet.absoluteFill}
-          id={`place.${place.key}.bg`}
-        >
-          <View style={s.bgStyle} />
-        </SharedElement>
+      <FlingGestureHandler
+        direction={Directions.DOWN}
+        onHandlerStateChange={handleSwipeDown}
+      >
+        <View style={s.containerStyle}>
+          <SharedElement
+            style={StyleSheet.absoluteFill}
+            id={`place.${place.key}.bg`}
+          >
+            <View style={s.bgStyle} />
+          </SharedElement>
 
-        <Animatable.View useNativeDriver ref={textRef} animation='bounceIn' delay={600} style={styles.fullWidth}>
-          <Text style={textStyles.boldOfSize(24)}>{place.title}</Text>
+          <Animatable.View
+            useNativeDriver
+            ref={textRef}
+            animation="bounceIn"
+            delay={600}
+            style={styles.fullWidth}
+          >
+            <Text style={textStyles.boldOfSize(24)}>{place.title}</Text>
 
-          <Text style={textStyles.normalOfSize(24)}>
-            {strings.distanceFromYou(place.distance)}
-          </Text>
-        </Animatable.View>
+            <Text style={textStyles.normalOfSize(24)}>
+              {strings.distanceFromYou(place.distance)}
+            </Text>
+          </Animatable.View>
 
-        <Animatable.View useNativeDriver ref={ratingRef} animation='bounceIn' delay={800} style={s.ratingContainer}>
-          <PlaceRating
-            locked
-            title={strings.placeScreen.crowdnessTitle}
-            image={require("../../assets/images/HowBusyOrange.png")}
-            color={colors.desertRock}
-            rating={place.crowdness}
-          />
+          <Animatable.View
+            useNativeDriver
+            ref={ratingRef}
+            animation="bounceIn"
+            delay={800}
+            style={s.ratingContainer}
+          >
+            <PlaceRating
+              locked
+              title={strings.placeScreen.crowdnessTitle}
+              image={require("../../assets/images/HowBusyOrange.png")}
+              color={colors.desertRock}
+              rating={place.crowdness}
+            />
 
-          <PlaceRating
-            locked
-            leftMargin={40}
-            title={strings.placeScreen.cleannessTitle(locked)}
-            image={require("../../assets/images/HeartL.png")}
-            color={colors.grass}
-            rating={place.cleanness}
-          />
-        </Animatable.View>
+            <PlaceRating
+              locked
+              leftMargin={40}
+              title={strings.placeScreen.cleannessTitle(locked)}
+              image={require("../../assets/images/HeartL.png")}
+              color={colors.grass}
+              rating={place.cleanness}
+            />
+          </Animatable.View>
 
-        <SharedElement style={s.imageStyle} id={`place.${place.key}.image`}>
-          <Image style={s.imageStyle} source={{ uri: place.image }} />
-        </SharedElement>
+          <SharedElement style={s.imageStyle} id={`place.${place.key}.image`}>
+            <Image style={s.imageStyle} source={{ uri: place.image }} />
+          </SharedElement>
 
-        <Animatable.View useNativeDriver animation='fadeInUp' delay={500} ref={visitorsRef} style={styles.fullWidth}>
-          <Text style={textStyles.normalOfSize(12)}>{strings.placeScreen.recentVisitors(locked)}</Text>
-          <View style={s.recentVisitorsContainer}>
-            {recentVisitors.map((visitor, index) => <RecentVisitor key={`${index}`} large title={visitor.name} details={visitor.role} image={visitor.image} /> )}
-          </View>
-        </Animatable.View>
+          <Animatable.View
+            useNativeDriver
+            animation="fadeInUp"
+            delay={500}
+            ref={visitorsRef}
+            style={styles.fullWidth}
+          >
+            <Text style={textStyles.normalOfSize(12)}>
+              {strings.placeScreen.recentVisitors(locked)}
+            </Text>
+            <View style={s.recentVisitorsContainer}>
+              {recentVisitors.map((visitor, index) => (
+                <RecentVisitor
+                  key={`${index}`}
+                  large
+                  title={visitor.name}
+                  details={visitor.role}
+                  image={visitor.image}
+                />
+              ))}
+            </View>
+          </Animatable.View>
 
-        <Animatable.Text useNativeDriver animation='fadeInUp' delay={700} ref={descRef} style={s.desc}>{place.description}</Animatable.Text>
-        
-        <Animatable.View useNativeDriver animation='fadeInUp' delay={900} ref={actionsRef} style={s.actions}>
+          <Animatable.Text
+            useNativeDriver
+            animation="fadeInUp"
+            delay={700}
+            ref={descRef}
+            style={s.desc}
+          >
+            {place.description}
+          </Animatable.Text>
 
-            <PlaceAction onPress={waze} title={strings.placeScreen.waze} icon={require("../../assets/images/waze_icon.png")} />
-            <PlaceAction onPress={share} title={strings.placeScreen.share} icon={require("../../assets/images/share_icon.png")} />
-            <PlaceAction onPress={report} title={strings.placeScreen.report} icon={require("../../assets/images/report_icon.png")} />
-        </Animatable.View>
-
-      </View>
+          <Animatable.View
+            useNativeDriver
+            animation="fadeInUp"
+            delay={900}
+            ref={actionsRef}
+            style={s.actions}
+          >
+            <PlaceAction
+              onPress={waze}
+              title={strings.placeScreen.waze}
+              icon={require("../../assets/images/waze_icon.png")}
+            />
+            <PlaceAction
+              onPress={share}
+              title={strings.placeScreen.share}
+              icon={require("../../assets/images/share_icon.png")}
+            />
+            <PlaceAction
+              onPress={report}
+              title={strings.placeScreen.report}
+              icon={require("../../assets/images/report_icon.png")}
+            />
+          </Animatable.View>
+        </View>
+      </FlingGestureHandler>
     </View>
   );
 };
 
-const PlaceAction = ({title, icon, onPress}) => {
+const PlaceAction = ({ title, icon, onPress }) => {
   return (
-    <TouchableOpacity onPress={onPress} style={{
-      alignItems: 'center',
-      justifyContent: 'flex-start'
-    }}>
+    <TouchableOpacity
+      onPress={onPress}
+      style={{
+        alignItems: "center",
+        justifyContent: "flex-start",
+      }}
+    >
       <Image source={icon} />
-      <Text style={{
-        ...textStyles.normalOfSize(12),
-        textAlign: 'center',
-        marginTop: 6
-      }}>{title}</Text>
+      <Text
+        style={{
+          ...textStyles.normalOfSize(12),
+          textAlign: "center",
+          marginTop: 6,
+        }}
+      >
+        {title}
+      </Text>
     </TouchableOpacity>
-  )
+  );
 };
 
 const PlaceRating = ({
@@ -165,24 +240,23 @@ const PlaceRating = ({
 };
 
 const s = StyleSheet.create({
-
   desc: {
     ...textStyles.normalOfSize(16),
     ...styles.fullWidth,
-    lineHeight: 17
+    lineHeight: 17,
   },
 
   actions: {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    alignItems: 'center',
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
     ...styles.fullWidth,
   },
 
   recentVisitorsContainer: {
     marginTop: 4,
-    justifyContent: 'flex-start',
-    flexDirection: 'row-reverse',
+    justifyContent: "flex-start",
+    flexDirection: "row-reverse",
     // backgroundColor: 'cyan'
   },
 
@@ -222,7 +296,7 @@ const s = StyleSheet.create({
   },
 
   imageStyle: {
-    resizeMode: 'cover',
+    resizeMode: "cover",
     borderRadius: 15,
     width: "100%",
     height: height * 0.27,
