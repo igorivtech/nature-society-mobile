6cace4397c2d10fd754231b346b03b8594b41580
Nimrod <nimrodbens@gmail.com>
2020-12-02 17:57:49 +0200

    refactor code

--- components/screens/LoginScreen.js
+++ components/screens/LoginScreen.js
@@ -1,13 +1,10 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
   View,
   Text,
   StyleSheet,
   TouchableOpacity,
   SafeAreaView,
-  TextInput,
-  Animated,
-  Easing,
   ScrollView,
   Keyboard,
 } from "react-native";
@@ -19,6 +16,7 @@ import { textStyles } from "../../values/textStyles";
 import { CoolButton } from "../views/onboarding/views";
 import * as Animatable from 'react-native-animatable';
 import {useKeyboard} from '../../hooks/useKeyboard'
+import { Input } from "../views/login/Input";
 
 export const LoginScreen = ({ navigation }) => {
 
@@ -130,90 +128,6 @@ const SmallButton = ({title, onPress}) => {
   )
 }
 
-const DURATION = 300;
-const TEXT_SCALE = 0.7;
-
-const Input = ({ autoCapitalize = 'words', keyboardType = 'default', title, value, onChange, secure = false, extraMargin = false }) => {
-  
-  const textTranslateY = useRef(new Animated.Value(0)).current;
-  const textTranslateX = useRef(new Animated.Value(0)).current;
-  const textScale = useRef(new Animated.Value(1)).current;
-  const placeholderWidth = useRef();
-  const placeholderHeight = useRef();
-
-  const onBlur = () => {
-    if (value.length === 0) {
-      Animated.parallel([
-        Animated.timing(textTranslateY, {
-          duration: DURATION,
-          useNativeDriver: true,
-          toValue: 0,
-          easing: Easing.inOut(Easing.ease),
-        }),
-        Animated.timing(textTranslateX, {
-          duration: DURATION,
-          useNativeDriver: true,
-          toValue: 0,
-          easing: Easing.inOut(Easing.ease),
-        }),
-        Animated.timing(textScale, {
-          duration: DURATION,
-          useNativeDriver: true,
-          toValue: 1,
-          easing: Easing.inOut(Easing.ease),
-        }),
-      ]).start();
-    } else {
-      // ?
-    }
-  };
-
-  const onFocus = () => {
-    Animated.parallel([
-      Animated.timing(textTranslateY, {
-        duration: DURATION,
-        useNativeDriver: true,
-        toValue: -30, // -placeholderHeight.current - 12,
-        easing: Easing.inOut(Easing.ease),
-      }),
-      Animated.timing(textTranslateX, {
-        duration: DURATION,
-        useNativeDriver: true,
-        toValue: placeholderWidth.current * (1 - TEXT_SCALE)/2,
-        easing: Easing.inOut(Easing.ease),
-      }),
-      Animated.timing(textScale, {
-        duration: DURATION,
-        useNativeDriver: true,
-        toValue: TEXT_SCALE,
-        easing: Easing.inOut(Easing.ease),
-      }),
-    ]).start();
-  };
-
-  return (
-    <View style={styles.animatedTextContainer(extraMargin ? 64 : 32)}>
-      <Animated.Text onLayout={(e) => {
-        placeholderWidth.current = e.nativeEvent.layout.width;
-        placeholderHeight.current = e.nativeEvent.layout.height;
-      }} style={styles.animatedPlaceholder(textTranslateX, textTranslateY, textScale)}>
-        {title}
-      </Animated.Text>
-      <TextInput
-        underlineColorAndroid={colors.clear}
-        autoCapitalize={autoCapitalize}
-        keyboardType={keyboardType}
-        onChangeText={onChange}
-        secureTextEntry={secure}
-        value={value}
-        onBlur={onBlur}
-        onFocus={onFocus}
-        style={styles.textInput}
-        selectionColor={colors.desertRock}
-      />
-    </View>
-  );
-};
 
 const styles = StyleSheet.create({
 
@@ -231,30 +145,6 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     color: colors.treeBlues
   },
-
-  textInput: {
-    ...textStyles.normalOfSize(18),
-    color: colors.treeBlues,
-  },
-
-  animatedPlaceholder: (translateX, translateY, scale) => ({
-    transform: [{ translateX }, { translateY }, { scale }],
-    position: "absolute",
-    right: 4,
-    bottom: 4,
-    ...textStyles.normalOfSize(18),
-    color: colors.treeBlues,
-    textDecorationLine: "underline",
-  }),
-
-  animatedTextContainer: (marginBottom) => ({
-    width: "100%",
-    paddingTop: 16,
-    borderBottomColor: colors.treeBlues,
-    borderBottomWidth: 1,
-    paddingBottom: 4,
-    marginBottom,
-  }),
 
   scrollView: (paddingBottom) => ({
     justifyContent: "center",
---
+++ components/views/login/Input.js
@@ -0,0 +1,119 @@
+import React, { useRef } from "react";
+import { Animated, Easing, StyleSheet, TextInput, View } from "react-native";
+import { colors } from "../../../values/colors";
+import { textStyles } from "../../../values/textStyles";
+
+const DURATION = 300;
+const TEXT_SCALE = 0.7;
+
+export const Input = ({ autoCapitalize = 'words', keyboardType = 'default', title, value, onChange, secure = false, extraMargin = false }) => {
+  
+  const textTranslateY = useRef(new Animated.Value(0)).current;
+  const textTranslateX = useRef(new Animated.Value(0)).current;
+  const textScale = useRef(new Animated.Value(1)).current;
+  const placeholderWidth = useRef();
+  const placeholderHeight = useRef();
+
+  const onBlur = () => {
+    if (value.length === 0) {
+      Animated.parallel([
+        Animated.timing(textTranslateY, {
+          duration: DURATION,
+          useNativeDriver: true,
+          toValue: 0,
+          easing: Easing.inOut(Easing.ease),
+        }),
+        Animated.timing(textTranslateX, {
+          duration: DURATION,
+          useNativeDriver: true,
+          toValue: 0,
+          easing: Easing.inOut(Easing.ease),
+        }),
+        Animated.timing(textScale, {
+          duration: DURATION,
+          useNativeDriver: true,
+          toValue: 1,
+          easing: Easing.inOut(Easing.ease),
+        }),
+      ]).start();
+    } else {
+      // ?
+    }
+  };
+
+  const onFocus = () => {
+    Animated.parallel([
+      Animated.timing(textTranslateY, {
+        duration: DURATION,
+        useNativeDriver: true,
+        toValue: -30, // -placeholderHeight.current - 12,
+        easing: Easing.inOut(Easing.ease),
+      }),
+      Animated.timing(textTranslateX, {
+        duration: DURATION,
+        useNativeDriver: true,
+        toValue: placeholderWidth.current * (1 - TEXT_SCALE)/2,
+        easing: Easing.inOut(Easing.ease),
+      }),
+      Animated.timing(textScale, {
+        duration: DURATION,
+        useNativeDriver: true,
+        toValue: TEXT_SCALE,
+        easing: Easing.inOut(Easing.ease),
+      }),
+    ]).start();
+  };
+
+  return (
+    <View style={styles.animatedTextContainer(extraMargin ? 64 : 32)}>
+      <Animated.Text onLayout={(e) => {
+        placeholderWidth.current = e.nativeEvent.layout.width;
+        placeholderHeight.current = e.nativeEvent.layout.height;
+      }} style={styles.animatedPlaceholder(textTranslateX, textTranslateY, textScale)}>
+        {title}
+      </Animated.Text>
+      <TextInput
+        underlineColorAndroid={colors.clear}
+        autoCapitalize={autoCapitalize}
+        keyboardType={keyboardType}
+        onChangeText={onChange}
+        secureTextEntry={secure}
+        value={value}
+        onBlur={onBlur}
+        onFocus={onFocus}
+        style={styles.textInput}
+        selectionColor={colors.desertRock}
+      />
+    </View>
+  );
+};
+
+
+const styles = StyleSheet.create({
+
+    textInput: {
+      ...textStyles.normalOfSize(18),
+      color: colors.treeBlues,
+    },
+  
+    animatedPlaceholder: (translateX, translateY, scale) => ({
+      transform: [{ translateX }, { translateY }, { scale }],
+      position: "absolute",
+      right: 4,
+      bottom: 4,
+      ...textStyles.normalOfSize(18),
+      color: colors.treeBlues,
+      textDecorationLine: "underline",
+    }),
+  
+    animatedTextContainer: (marginBottom) => ({
+      width: "100%",
+      paddingTop: 16,
+      borderBottomColor: colors.treeBlues,
+      borderBottomWidth: 1,
+      paddingBottom: 4,
+      marginBottom,
+    }),
+  
+  });
+  
