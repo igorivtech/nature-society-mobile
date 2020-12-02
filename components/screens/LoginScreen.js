cb9f732e894e3b9c7cd2e8dad060f7e92a145f5d
Nimrod <nimrodbens@gmail.com>
2020-12-02 20:06:57 +0200

    commented out lines

--- components/screens/LoginScreen.js
+++ components/screens/LoginScreen.js
@@ -91,11 +91,11 @@ export const LoginScreen = ({ navigation }) => {
     setLoadingImage(true);
     let result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.All,
-      //allowsEditing: true,
-      //aspect: [4, 3],
+      // allowsEditing: true,
+      // aspect: [4, 3],
       quality: 0.75,
     });
-    console.log(result);
+    // console.log(result);
     if (!result.cancelled) {
       setImage(result);
     }
