f05a753fcd19adbe452538e91f827af81006a40f
Nimrod <nimrodbens@gmail.com>
2020-12-02 12:07:32 +0200

    added growth points text animation

--- components/views/home/GrowthPoints.js
+++ components/views/home/GrowthPoints.js
@@ -5,41 +5,66 @@ import { styles } from "../../../values/styles";
 import { textStyles } from "../../../values/textStyles";
 
 const animation = {
-    0: {
-      opacity: 1,
-      transform: [
-        {translateX: 0},
-        {scale: 1},
-      ]
-    },
-    1: {
-      opacity: 0,
-      transform: [
-        {translateX: 130/2},
-        {scale: 0},
-      ]
-    }
+  0: {
+    opacity: 1,
+    transform: [
+      {translateX: 0},
+      {scale: 1},
+    ]
+  },
+  1: {
+    opacity: 0,
+    transform: [
+      {translateX: 130/2},
+      {scale: 0},
+    ]
   }
+}
+
+const opacityOff = {
+  0: {
+    opacity: 1,
+  },
+  1: {
+    opacity: 0,
+  }
+}
+const opacityOn = {
+  0: {
+    opacity: 0,
+  },
+  1: {
+    opacity: 1,
+  }
+}
+
+const TEXT_DURATION = 500;
 
 export const GrowthPoints = () => {
 
     const [points, setPoints] = useState(0);
-    const ref = useRef();
+    const containerRef = useRef();
+    const textRef = useRef();
   
     useEffect(()=>{
       setTimeout(() => {
-        setPoints(100);
-        setTimeout(() => {
-          ref.current.animate(animation, 800);
-        }, 2000);
+        textRef.current.animate(opacityOff, TEXT_DURATION).then(()=>{
+          setPoints(100);
+          textRef.current.animate(opacityOn, TEXT_DURATION).then(()=>{
+            setTimeout(() => {
+              containerRef.current.animate(animation, 800);
+            }, 4000);
+          })
+        })
       }, 2000);
     }, [])
   
-    return (<Animatable.View ref={ref} style={styles.pointsGrowthContainer}>
-      <Text 
+    return (<Animatable.View ref={containerRef} style={styles.pointsGrowthContainer}>
+      <Animatable.Text 
+      ref={textRef}
       adjustsFontSizeToFit={true}
       numberOfLines={1}
-      style={textStyles.pointsGrowthText} >{points}</Text>
+      style={textStyles.pointsGrowthText} >{points}</Animatable.Text>
       <Image source={require("../../../assets/images/growth_icon.png")} />
   
     </Animatable.View>)
