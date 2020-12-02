45f69cc162bd7326abc7d2105569c372a207a592
Nimrod <nimrodbens@gmail.com>
2020-12-02 18:05:25 +0200

    refactor code

--- components/screens/LoginScreen.js
+++ components/screens/LoginScreen.js
@@ -1,22 +1,15 @@
 import React, { useEffect, useState } from "react";
 import {
   View,
-  Text,
   StyleSheet,
-  TouchableOpacity,
   SafeAreaView,
   ScrollView,
   Keyboard,
 } from "react-native";
 import { State, TapGestureHandler } from "react-native-gesture-handler";
 import { colors } from "../../values/colors";
-import { CARD_RADIUS, width } from "../../values/consts";
-import { strings } from "../../values/strings";
-import { textStyles } from "../../values/textStyles";
-import { CoolButton } from "../views/onboarding/views";
-import * as Animatable from 'react-native-animatable';
 import {useKeyboard} from '../../hooks/useKeyboard'
-import { Input } from "../views/login/Input";
+import { LoginView } from "../views/login/views";
 
 export const LoginScreen = ({ navigation }) => {
 
@@ -86,65 +79,9 @@ export const LoginScreen = ({ navigation }) => {
   );
 };
 
-const LoginView = ({email, onEmailChanged, password, onPasswordChanged, forgotPassword, login, signup}) => {
-  return (
-    <Animatable.View animation='fadeInUpBig' style={styles.cardContainer}>
-      <Text style={styles.loginTitle}>{strings.loginScreen.title}</Text>
-
-      <Input
-        autoCapitalize='none'
-        keyboardType='email-address'
-        title={strings.email}
-        onChange={onEmailChanged}
-        value={email}
-      />
-      <Input
-        extraMargin={true}
-        autoCapitalize='none'
-        title={strings.password}
-        onChange={onPasswordChanged}
-        value={password}
-        secure={true}
-      />
-
-      <CoolButton textStyle={{
-        ...textStyles.boldOfSize(24),
-        color: 'white'
-      }} title={strings.login} onPress={login} />
-
-      <View style={styles.bottomButtonsContainer}>
-        <SmallButton onPress={forgotPassword} title={strings.loginScreen.forgotPassword} />
-        <SmallButton onPress={signup} title={strings.loginScreen.signup} />
-      </View>
-    </Animatable.View>
-  )
-}
-
-const SmallButton = ({title, onPress}) => {
-  return (
-    <TouchableOpacity onPress={onPress}>
-      <Text style={styles.smallButtonText}>{title}</Text>
-    </TouchableOpacity>
-  )
-}
 
 
 const styles = StyleSheet.create({
-
-  bottomButtonsContainer: {
-    marginTop: 18,
-    flexDirection: 'row',
-    justifyContent: 'space-evenly',
-    alignItems: 'center',
-    width: '100%'
-  },
-
-  smallButtonText: {
-    textDecorationLine: 'underline',
-    ...textStyles.normalOfSize(12),
-    textAlign: 'center',
-    color: colors.treeBlues
-  },
 
   scrollView: (paddingBottom) => ({
     justifyContent: "center",
@@ -152,32 +89,6 @@ const styles = StyleSheet.create({
     paddingBottom
   }),
 
-  loginTitle: {
-    marginBottom: 38,
-    ...textStyles.boldOfSize(24),
-    color: colors.treeBlues,
-    textAlign: "right",
-    width: "100%",
-  },
-
-  cardContainer: {
-    alignItems: "center",
-    justifyContent: "space-between",
-    paddingTop: 32,
-    paddingBottom: 24,
-    paddingHorizontal: 40,
-    width: width - 2 * 30,
-    alignSelf: "center",
-    backgroundColor: "white",
-    borderRadius: CARD_RADIUS,
-    shadowOffset: {
-      height: -4,
-      width: 0,
-    },
-    shadowColor: "rgba(0, 0, 0, 0.035)",
-    shadowRadius: 12,
-    shadowOpacity: 1,
-  },
   container: {
     flex: 1,
     backgroundColor: colors.grass,
---
+++ components/views/login/views.js
@@ -0,0 +1,112 @@
+import React from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import * as Animatable from 'react-native-animatable';
+import { strings } from '../../../values/strings';
+import { textStyles } from '../../../values/textStyles';
+import { Input } from './Input';
+import { CoolButton } from "../onboarding/views";
+import { colors } from '../../../values/colors';
+import { CARD_RADIUS, width } from '../../../values/consts';
+
+export const LoginView = ({
+  email,
+  onEmailChanged,
+  password,
+  onPasswordChanged,
+  forgotPassword,
+  login,
+  signup,
+}) => {
+  return (
+    <Animatable.View animation="fadeInUpBig" style={styles.cardContainer}>
+      <Text style={styles.loginTitle}>{strings.loginScreen.title}</Text>
+
+      <Input
+        autoCapitalize="none"
+        keyboardType="email-address"
+        title={strings.email}
+        onChange={onEmailChanged}
+        value={email}
+      />
+      <Input
+        extraMargin={true}
+        autoCapitalize="none"
+        title={strings.password}
+        onChange={onPasswordChanged}
+        value={password}
+        secure={true}
+      />
+
+      <CoolButton
+        textStyle={{
+          ...textStyles.boldOfSize(24),
+          color: "white",
+        }}
+        title={strings.login}
+        onPress={login}
+      />
+
+      <View style={styles.bottomButtonsContainer}>
+        <SmallButton
+          onPress={forgotPassword}
+          title={strings.loginScreen.forgotPassword}
+        />
+        <SmallButton onPress={signup} title={strings.loginScreen.signup} />
+      </View>
+    </Animatable.View>
+  );
+};
+
+export const SmallButton = ({ title, onPress }) => {
+  return (
+    <TouchableOpacity onPress={onPress}>
+      <Text style={styles.smallButtonText}>{title}</Text>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+
+    bottomButtonsContainer: {
+      marginTop: 18,
+      flexDirection: 'row',
+      justifyContent: 'space-evenly',
+      alignItems: 'center',
+      width: '100%'
+    },
+  
+    smallButtonText: {
+      textDecorationLine: 'underline',
+      ...textStyles.normalOfSize(12),
+      textAlign: 'center',
+      color: colors.treeBlues
+    },
+  
+    loginTitle: {
+      marginBottom: 38,
+      ...textStyles.boldOfSize(24),
+      color: colors.treeBlues,
+      textAlign: "right",
+      width: "100%",
+    },
+  
+    cardContainer: {
+      alignItems: "center",
+      justifyContent: "space-between",
+      paddingTop: 32,
+      paddingBottom: 24,
+      paddingHorizontal: 40,
+      width: width - 2 * 30,
+      alignSelf: "center",
+      backgroundColor: "white",
+      borderRadius: CARD_RADIUS,
+      shadowOffset: {
+        height: -4,
+        width: 0,
+      },
+      shadowColor: "rgba(0, 0, 0, 0.035)",
+      shadowRadius: 12,
+      shadowOpacity: 1,
+    },
+  });
+  
