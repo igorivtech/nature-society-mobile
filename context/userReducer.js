03b1223caae1b2b226d29b93df48c38d7db68880
Nimrod <nimrodbens@gmail.com>
2020-12-05 12:08:49 +0200

    refactor code

--- context/userReducer.js
+++ context/userReducer.js
@@ -1,30 +1,30 @@
 export const SAVE_USER = "SAVE_USER";
 
 export const initialState = {
-    token: null,
-    // user: null,
-    user: {
-      name: "יעל השכנה",
-      email: "yael@nextdoor",
-      image: "https://cdn.iconscout.com/icon/premium/png-256-thumb/woman-avatar-1543937-1371628.png"
-    },
-    // fetchingUser: false,
-    // pushToken: null,
-    // isSignout: false,
-    // pushOn: null,
-    // liveUpdatesOn: false
-  };
+  token: null,
+  // user: null,
+  user: {
+    name: "יעל השכנה",
+    email: "yael@nextdoor",
+    image:
+      "https://cdn.iconscout.com/icon/premium/png-256-thumb/woman-avatar-1543937-1371628.png",
+  },
+  // fetchingUser: false,
+  // pushToken: null,
+  // isSignout: false,
+  // pushOn: null,
+  // liveUpdatesOn: false
+};
 
-  export const reducer = (state, action) => {
-    switch (action.type) {
-      case SAVE_USER:
-        return {
-          ...state,
-          user: action.payload,
-          fetchingUser: false,
-        };
-      default:
-        return state;
-    }
-  };
-  
+export const reducer = (state, action) => {
+  switch (action.type) {
+    case SAVE_USER:
+      return {
+        ...state,
+        user: action.payload,
+        fetchingUser: false,
+      };
+    default:
+      return state;
+  }
+};
