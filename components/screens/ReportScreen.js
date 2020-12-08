85ed40833767c04bf36f085090ec310140eff0a8
Nimrod <nimrodbens@gmail.com>
2020-12-08 13:45:49 +0200

    working on report screen

--- components/screens/ReportScreen.js
+++ components/screens/ReportScreen.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useRef } from "react";
-import { View, Text, StyleSheet, TouchableWithoutFeedback, Animated, Easing } from "react-native";
+import { View, Text, StyleSheet, TouchableWithoutFeedback, Animated, Easing, SafeAreaView } from "react-native";
 import { globalStyles } from "../../values/styles";
 import LottieView from 'lottie-react-native';
+import { colors } from "../../values/colors";
 
 export const ReportScreen = ({navigation}) => {
 
@@ -17,13 +18,28 @@ export const ReportScreen = ({navigation}) => {
   }
 
   return (
-    <View style={globalStyles.container}>
-      <LottieView source={require('../../assets/animations/rainbow.json')} progress={progress} resizeMode='contain' />
-      <TouchableWithoutFeedback style={StyleSheet.absoluteFill} onPress={animate}>
-        <Text style={{
-          transform: [{translateY: -100}]
-        }}>ReportScreen</Text>
-      </TouchableWithoutFeedback>
-    </View>
+    <SafeAreaView style={{
+      ...globalStyles.container,
+      backgroundColor: colors.grass,
+      paddingHorizontal: 100
+    }}>
+      <View style={{
+        backgroundColor: 'white',
+        flex: 1,
+        alignSelf: 'stretch',
+        justifyContent: 'center',
+        alignItems: 'center'
+      }}>
+
+        <LottieView source={require('../../assets/animations/rainbow.json')} progress={progress} resizeMode='contain' />
+        <TouchableWithoutFeedback onPress={animate}>
+          <Text style={{
+            transform: [{translateY: -100}],
+            color: colors.darkWithTone,
+          }}>ReportScreen</Text>
+        </TouchableWithoutFeedback>
+
+      </View>
+    </SafeAreaView>
   );
 };
