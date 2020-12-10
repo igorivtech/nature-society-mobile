b6a5f448e0f84dbe6176bbd7312ed8e5a9e75501
Nimrod <nimrodbens@gmail.com>
2020-12-10 12:54:40 +0200

    working on report view

--- components/views/report/Report.js
+++ components/views/report/Report.js
@@ -1,30 +1,55 @@
 import React from "react";
-import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import { View, Text, TouchableOpacity, StyleSheet, Image } from "react-native";
+import { colors } from "../../../values/colors";
+import { strings } from "../../../values/strings";
+import { textStyles } from "../../../values/textStyles";
 import { Pagination } from "./Slider";
 
 export const Report = ({goBack}) => {
   return (
     <View style={styles.container}>
-      <View style={styles.pagContainer}>
-        <Pagination index={2} />
+      <View style={styles.firstContainer}>
+        <TouchableOpacity onPress={goBack} style={styles.goBackButton}>
+          <Image source={require("../../../assets/images/scroll_back_icon.png")} />
+          <Text style={styles.goBack}>{strings.reportScreen.goBack}</Text>
+        </TouchableOpacity>
+        <View style={styles.pagContainer}>
+          <Pagination index={2} />
+        </View>
       </View>
-      <TouchableOpacity onPress={goBack}>
-        <Text>Report</Text>
-      </TouchableOpacity>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+
+  firstContainer: {
+    flex: 1
+  },
+
   container: {
     height: "33.3333333333333%",
     width: "100%",
-    alignItems: "center",
-    justifyContent: "center",
+    alignItems: 'stretch',
+    justifyContent: 'flex-start',
+    paddingTop: 13,
+    paddingBottom: 21,
+    paddingHorizontal: 30
   },
   pagContainer: {
     position: 'absolute',
     top: 55,
-    left: 30
-  }
+  },
+
+  goBackButton: {
+    alignItems: 'center',
+    justifyContent: 'space-between'
+  },
+
+  goBack: {
+    marginTop: 0,
+    ...textStyles.normalOfSize(12),
+    textAlign: 'center',
+    color: colors.lighterShade
+  },
 });
