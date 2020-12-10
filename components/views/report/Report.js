cee64c848517fb31df34edad877cf16b8a234f68
Nimrod <nimrodbens@gmail.com>
2020-12-10 13:32:53 +0200

    added take pic view in report screen

--- components/views/report/Report.js
+++ components/views/report/Report.js
@@ -1,11 +1,15 @@
-import React from "react";
-import { View, Text, TouchableOpacity, StyleSheet, Image } from "react-native";
+import React, { useState } from "react";
+import { View, Text, TouchableOpacity, StyleSheet, Image, ActivityIndicator } from "react-native";
 import { colors } from "../../../values/colors";
 import { strings } from "../../../values/strings";
 import { textStyles } from "../../../values/textStyles";
 import { Pagination } from "./Slider";
+import * as ImagePicker from 'expo-image-picker';
 
 export const Report = ({goBack}) => {
+
+  const [image, setImage] = useState(null);
+
   return (
     <View style={styles.container}>
       <View style={styles.firstContainer}>
@@ -17,10 +21,72 @@ export const Report = ({goBack}) => {
           <Text style={textStyles.normalOfSize(12)}>{strings.reportScreen.takePic1}</Text>
           <Text style={textStyles.normalOfSize(18)}>{strings.reportScreen.takePic2}</Text>
         </View>
+        <TakePicView image={image} setImage={setImage} />
       </View>
     </View>
   );
 };
+
+const TakePicView = ({image, setImage}) => {
+
+  const [loadingImage, setLoadingImage] = useState(false);
+
+  const selectImage = async () => {
+    setLoadingImage(true);
+    let result = await ImagePicker.launchCameraAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      // allowsEditing: true,
+      // aspect: [4, 3],
+      quality: 0.75,
+    });
+    // console.log(result);
+    if (!result.cancelled) {
+      setImage(result);
+    }
+    setLoadingImage(false);
+  };
+
+  return (
+    <TouchableOpacity onPress={selectImage} style={cameraStyle.container(image)}>
+      <Image source={require("../../../assets/images/camera_icon.png")} />
+      
+      {loadingImage && (
+        <View style={cameraStyle.indicatorContainer}>
+          <ActivityIndicator style={cameraStyle.indicator} color={colors.treeBlues} />
+        </View>
+      )}
+      {image && (<Image style={cameraStyle.image} source={{uri: image.uri}} />)}
+    </TouchableOpacity>
+  )
+}
+
+const cameraStyle = StyleSheet.create({
+  indicatorContainer: {
+    ...StyleSheet.absoluteFill,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  indicator: {
+    transform: [
+      {translateY: 60}
+    ]
+  },
+  image: {
+    ...StyleSheet.absoluteFill,
+    borderRadius: 15
+  },
+  container: (image) => ({
+    width: "100%",
+    borderWidth: image !== null ? 0 : 1,
+    borderColor: colors.treeBlues,
+    aspectRatio: 1,
+    borderRadius: 15,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 22,
+    overflow: 'hidden'
+  })
+});
 
 const GoBackButton = ({goBack}) => {
   return (
@@ -38,7 +104,8 @@ const styles = StyleSheet.create({
   },
 
   firstContainer: {
-    flex: 1
+    flex: 1,
+    alignItems: 'stretch'
   },
 
   container: {
