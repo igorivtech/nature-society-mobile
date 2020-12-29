60512254062d9833e06dd169313926f0b8868545
Nimrod <nimrodbens@gmail.com>
2020-12-29 22:36:13 +0200

    async upload image

--- hooks/aws.js
+++ hooks/aws.js
@@ -3,6 +3,7 @@ import { v4 as uuidv4 } from "uuid";
 import { Storage } from "aws-amplify";
 import { useContext } from "react";
 import { UserContext } from "../context/context";
+import {BASE_URL} from './useServer'
 
 export const useUploadImage = () => {
   
@@ -42,17 +43,29 @@ export const uploadImageAsync = async (token, reportId, image) => {
   if (image == null || token == null || reportId == null) {
     return;
   }
-  const fileName = uuidv4();
-  const result = image;
-  const response = await fetch(result.uri);
-  const blob = await response.blob();
-  Storage.put(`users/${fileName}`, blob, {
-    contentType: image.type,
-    ACL: "public-read",
-    visibility: "public",
-    level: "public",
-  })
-    .then(() => {
-      const url = `https://naturesocietyd770eeed1aeb4e34ba859038a344c121174124-prod.s3.eu-central-1.amazonaws.com/public/users/${fileName}`;
+  try {
+    const fileName = uuidv4();
+    const result = image;
+    const response = await fetch(result.uri);
+    const blob = await response.blob();
+    await Storage.put(`users/${fileName}`, blob, {
+      contentType: image.type,
+      ACL: "public-read",
+      visibility: "public",
+      level: "public",
     })
+    const url = `https://naturesocietyd770eeed1aeb4e34ba859038a344c121174124-prod.s3.eu-central-1.amazonaws.com/public/users/${fileName}`;
+    await fetch(`${BASE_URL}/editReport`, {
+      method: "PATCH",
+      body: JSON.stringify({
+        id: reportId,
+        image: url
+      }),
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        Authorization: token
+      }
+    });
+  } catch (err) {console.error(err)}
 }
