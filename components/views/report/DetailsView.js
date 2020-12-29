3b22d48e94a51406ebdcaa4626242b739111cf30
Nimrod <nimrodbens@gmail.com>
2020-12-29 22:01:09 +0200

    SE layout

--- components/views/report/DetailsView.js
+++ components/views/report/DetailsView.js
@@ -7,14 +7,15 @@ import { textStyles } from "../../../values/textStyles";
 
 export const DetailsView = ({details, iHelped}) => {
     return (
-      <View style={detailsStyles.container}>
-        <Text style={textStyles.normalOfSize(18)}>{strings.reportScreen.additionalInfo}</Text>
+      <View style={[detailsStyles.container]}>
+        <Text style={textStyles.normalOfSize(smallScreen ? 16 : 18)}>{strings.reportScreen.additionalInfo}</Text>
         <View style={detailsStyles.checkboxesContainer}>
           {details.map(detail => <Checkbox key={detail.id} detail={detail} />)}
         </View>
         <View style={{
           alignItems: 'flex-end',
-          marginVertical: 8,
+          marginVertical: smallScreen ? 0 : 8,
+          marginTop: smallScreen ? 4 : 0
         }}>
           <Checkbox large={true} detail={iHelped} />
         </View>
@@ -23,7 +24,7 @@ export const DetailsView = ({details, iHelped}) => {
   }
   
   const CHECKBOX_SIZE = 22.5;
-  const CHECKBOX_SIZE_LARGE = 44;
+  const CHECKBOX_SIZE_LARGE = smallScreen ? 40 : 44;
   
   export const Checkbox = ({detail, large = false}) => {
   
@@ -61,7 +62,7 @@ export const DetailsView = ({details, iHelped}) => {
   const detailsStyles = StyleSheet.create({
   
     checkboxesContainer: {
-      marginTop: 8,
+      marginTop: smallScreen ? 12 : 8,
       flexDirection: 'row',
       flexWrap: 'wrap',
       justifyContent: 'flex-end'
@@ -90,6 +91,7 @@ export const DetailsView = ({details, iHelped}) => {
     }),
     container: {
       marginVertical: smallScreen ? 12 : 16,
+      marginBottom: smallScreen ? 6 : 16,
       flex: 1,
       alignItems: 'stretch',
       justifyContent: 'space-around'
