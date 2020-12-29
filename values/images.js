2c3a7a52d23b57e037f8970649139d8494118aef
Nimrod <nimrodbens@gmail.com>
2020-12-29 14:30:00 +0200

    added rankings titles

--- values/images.js
+++ values/images.js
@@ -2,82 +2,102 @@ const tempRankImages = [
   {
     on: require("../assets/images/rank_0_on.png"),
     off: require("../assets/images/rank_0_off.png"),
+    title: "אוהב טבע"
   },
   {
     on: require("../assets/images/rank_1_on.png"),
     off: require("../assets/images/rank_1_off.png"),
+    title: "אוהב טבע מתקדם"
   },
   {
     on: require("../assets/images/rank_2_on.png"),
     off: require("../assets/images/rank_2_off.png"),
+    title: "אוהב טבע מקצוען"
   },
   {
     on: require("../assets/images/rank_3_on.png"),
     off: require("../assets/images/rank_3_off.png"),
+    title: "אוהב טבע מומחה"
   },
   {
     on: require("../assets/images/rank_4_on.png"),
     off: require("../assets/images/rank_4_off.png"),
+    title: "פעיל טבע"
   },
   {
     on: require("../assets/images/rank_5_on.png"),
     off: require("../assets/images/rank_5_off.png"),
+    title: "פעיל טבע מתקדם"
   },
   {
     on: require("../assets/images/rank_6_on.png"),
     off: require("../assets/images/rank_6_off.png"),
+    title: "פעיל טבע מקצוען"
   },
   {
     on: require("../assets/images/rank_7_on.png"),
     off: require("../assets/images/rank_7_off.png"),
+    title: "פעיל טבע מומחה"
   },
   {
     on: require("../assets/images/rank_8_on.png"),
     off: require("../assets/images/rank_8_off.png"),
+    title: "נאמן טבע"
   },
   {
     on: require("../assets/images/rank_9_on.png"),
     off: require("../assets/images/rank_9_off.png"),
+    title: "נאמן טבע מתקדם"
   },
   {
     on: require("../assets/images/rank_10_on.png"),
     off: require("../assets/images/rank_10_off.png"),
+    title: "נאמן טבע מקצוען"
   },
   {
     on: require("../assets/images/rank_11_on.png"),
     off: require("../assets/images/rank_11_off.png"),
+    title: "נאמן טבע מומחה"
   },
   {
     on: require("../assets/images/rank_12_on.png"),
     off: require("../assets/images/rank_12_off.png"),
+    title: "שומר טבע"
   },
   {
     on: require("../assets/images/rank_13_on.png"),
     off: require("../assets/images/rank_13_off.png"),
+    title: "שומר טבע מתקדם"
   },
   {
     on: require("../assets/images/rank_14_on.png"),
     off: require("../assets/images/rank_14_off.png"),
+    title: "שומר טבע מקצוען"
   },
   {
     on: require("../assets/images/rank_15_on.png"),
     off: require("../assets/images/rank_15_off.png"),
+    title: "שומר טבע מומחה"
   },
   {
     on: require("../assets/images/rank_16_on.png"),
     off: require("../assets/images/rank_16_off.png"),
+    title: "לוחם טבע"
   },
   {
     on: require("../assets/images/rank_17_on.png"),
     off: require("../assets/images/rank_17_off.png"),
+    title: "לוחם טבע מתקדם"
   },
   {
     on: require("../assets/images/rank_18_on.png"),
     off: require("../assets/images/rank_18_off.png"),
+    title: "לוחם טבע מקצוען"
   },
   {
     on: require("../assets/images/rank_19_on.png"),
     off: require("../assets/images/rank_19_off.png"),
+    title: "לוחם טבע מומחה"
   },
 ];
 
