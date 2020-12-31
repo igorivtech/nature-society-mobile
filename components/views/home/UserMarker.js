056d01a153dae97aadaa12bfbdf1a42bdfe929cd
Nimrod <nimrodbens@gmail.com>
2020-12-31 22:03:09 +0200

    added user marker default avatar

--- components/views/home/UserMarker.js
+++ components/views/home/UserMarker.js
@@ -1,19 +1,24 @@
 import React, { useState, useCallback, useEffect } from "react";
 import { Image, Platform, StyleSheet, View } from "react-native";
 import { Marker } from "react-native-maps";
+import { colors } from "../../../values/colors";
 
 const isAndroid = Platform.OS === 'android'
 
 export const UserMarker = ({ user, location }) => {
+  const [trackNumber, setTrackNumber] = useState(0);
   const [trackChanges, setTrackChanges] = useState(0);
+  
   const updateTrackChanged = useCallback(() => {
     setTrackChanges(v=>(v + 1));
   }, [setTrackChanges]);
   useEffect(()=>{
     setTrackChanges(0);
+    setTrackNumber(user !== null && user.image != null ? 3 : 2);
   }, [user])
+
   return (
-    <Marker style={markerStyles.markerContainer} zIndex={4} tracksViewChanges={isAndroid ? true : trackChanges < 2} coordinate={location}>
+    <Marker style={markerStyles.markerContainer} zIndex={4} tracksViewChanges={isAndroid ? true : trackChanges < trackNumber} coordinate={location}>
       <View style={markerStyles.markerInnerContainer}>
         <Image
           onLoad={updateTrackChanged}
@@ -22,9 +27,16 @@ export const UserMarker = ({ user, location }) => {
         />
         <Image
           onLoad={updateTrackChanged}
-          source={(user !== null && user.image != null) ? { uri: user.image } : require("../../../assets/images/default_profile_pic.png")}
-          style={markerStyles.profilePic}
+          source={require("../../../assets/images/default_profile_pic.png")}
+          style={markerStyles.avatar}
         />
+        {user !== null && user.image != null && (
+          <Image
+            onLoad={updateTrackChanged}
+            source={{ uri: user.image }}
+            style={markerStyles.profilePic}
+          />
+        )}
       </View>
     </Marker>
   );
@@ -41,10 +53,20 @@ export const markerStyles = StyleSheet.create({
     position: 'absolute'
   },
 
+  avatar: {
+    position: 'absolute',
+    top: 11,
+    transform: [{translateX: -0.1}],
+    backgroundColor: 'white',
+    width: 31.2,
+    height: 31.2,
+    borderRadius: 31.2/2
+  },
+
   profilePic: {
     marginTop: 11,
     marginRight: 0.5,
-    backgroundColor: 'white',
+    backgroundColor: colors.clear,
     width: 31.2,
     height: 31.2,
     borderRadius: 31.2/2
