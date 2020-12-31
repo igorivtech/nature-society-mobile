e2d543ce9482074cb6d90cebf72283e6d4411cca
Nimrod <nimrodbens@gmail.com>
2020-12-31 19:14:39 +0200

    crowd intro

--- components/screens/ReportScreen.js
+++ components/screens/ReportScreen.js
@@ -34,7 +34,7 @@ const crowd = {
   animation: require('../../assets/animations/crowd.json'),
   bg: require("../../assets/images/crowd_bg.png"),
   introAnimation: require('../../assets/animations/crowd_intro.json'),
-  outputRange: [0, 0.27184466019, 0.50485436893, 0.73786407767, 1]
+  outputRange: [0, 0.22321428571, 0.57142857142, 0.77678571428, 1]
 }
 
 export const ReportScreen = ({navigation, route}) => {
