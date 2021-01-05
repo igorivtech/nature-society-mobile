c6309608c02b9424f3ac6f15ff7c36ef5810ddc1
Nimrod <nimrodbens@gmail.com>
2021-01-05 13:44:09 +0200

    refactor logo

--- components/views/home/LogoView.js
+++ components/views/home/LogoView.js
@@ -1,17 +1,17 @@
-import React, {memo} from "react";
-import { Animated, SafeAreaView, StyleSheet } from "react-native";
-import { CARD_TRANSLATE_Y, ITEM_HEIGHT, SPACER_ITEM_SIZE } from "./PlaceCard";
+import React, { memo } from "react";
+import { Image } from "react-native";
+import { SPACER_ITEM_SIZE } from "./PlaceCard";
 
 export const LogoView = memo(() => {
   return (
-    <Animated.Image
-        pointerEvents='none'
-        source={require("../../../assets/images/hala_logo.png")}
-        style={{
-          position: 'absolute',
-          top: -(38 + 12),
-          left: SPACER_ITEM_SIZE * 1.25,
-        }}
-      />
+    <Image
+      pointerEvents="none"
+      source={require("../../../assets/images/hala_logo.png")}
+      style={{
+        position: "absolute",
+        top: -(38 + 12),
+        left: SPACER_ITEM_SIZE * 1.25,
+      }}
+    />
   );
 });
