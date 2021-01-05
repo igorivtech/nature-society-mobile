4d0fbbf1993d93749947e80f3253c0b86c1bb50b
Nimrod <nimrodbens@gmail.com>
2021-01-05 13:04:10 +0200

    added different distance func

--- hooks/helpers.js
+++ hooks/helpers.js
@@ -264,12 +264,27 @@ const firstNameOnly = (fullName) => {
 //   "title": " ",
 //   "type": " ",
 // },
-
+function toRad(Value) {
+    return Value * Math.PI / 180;
+}
 const distance = (lat1, lon1, lat2, lon2) => {
 	if ((lat1 == lat2) && (lon1 == lon2)) {
 		return 0;
 	}
 	else {
+
+    // var R = 6371; // km
+    // var dLat = toRad(lat2-lat1);
+    // var dLon = toRad(lon2-lon1);
+    // var lat1 = toRad(lat1);
+    // var lat2 = toRad(lat2);
+
+    // var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+    //   Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2); 
+    // var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+    // var d = R * c;
+    // return d;
+
 		var radlat1 = Math.PI * lat1/180;
 		var radlat2 = Math.PI * lat2/180;
 		var theta = lon1-lon2;
