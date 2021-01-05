80d96746bbf27d67b819f1f4c07b1b1eb26b8452
Nimrod <nimrodbens@gmail.com>
2021-01-05 15:50:54 +0200

    added website popup

--- components/views/home/LogoView.js
+++ components/views/home/LogoView.js
@@ -1,16 +1,28 @@
-import React, { memo } from "react";
+import React, { memo, useRef, useState } from "react";
 import { Image, StyleSheet, TouchableOpacity } from "react-native";
 import { SPACER_ITEM_SIZE } from "./PlaceCard";
 import * as WebBrowser from 'expo-web-browser';
 import * as Animatable from "react-native-animatable";
+import {Popup} from "../Popup"
+import { strings } from "../../../values/strings";
 
 export const LogoView = memo(() => {
+
+  const [popupVisible, setPopupVisible] = useState(false);
+
   const onPress = () => {
     WebBrowser.openBrowserAsync('https://www.teva.org.il');
   }
+
+  const actionRef = useRef(onPress);
+
+  const localOnPress = () => {
+    setPopupVisible(true);
+  }
   return (
-    <TouchableOpacity style={styles.logo} onPress={onPress}>
+    <TouchableOpacity style={styles.logo} onPress={localOnPress}>
       <Animatable.Image animation='fadeIn' delay={2000} source={require("../../../assets/images/hala_logo.png")} />
+      <Popup textData={strings.popups.halaWebsite} popupVisible={popupVisible} setPopupVisible={setPopupVisible} actionRef={actionRef} />
     </TouchableOpacity>
   );
 });
