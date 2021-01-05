5d2dd8119d7161589ab354bbf0c6933d0d673e43
Nimrod <nimrodbens@gmail.com>
2021-01-05 13:04:42 +0200

    changed distance function

--- hooks/helpers.js
+++ hooks/helpers.js
@@ -273,33 +273,33 @@ const distance = (lat1, lon1, lat2, lon2) => {
 	}
 	else {
 
-    // var R = 6371; // km
-    // var dLat = toRad(lat2-lat1);
-    // var dLon = toRad(lon2-lon1);
-    // var lat1 = toRad(lat1);
-    // var lat2 = toRad(lat2);
+    var R = 6371; // km
+    var dLat = toRad(lat2-lat1);
+    var dLon = toRad(lon2-lon1);
+    var lat1 = toRad(lat1);
+    var lat2 = toRad(lat2);
 
-    // var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
-    //   Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2); 
-    // var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
-    // var d = R * c;
-    // return d;
+    var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+      Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2); 
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+    var d = R * c;
+    return d;
 
-		var radlat1 = Math.PI * lat1/180;
-		var radlat2 = Math.PI * lat2/180;
-		var theta = lon1-lon2;
-		var radtheta = Math.PI * theta/180;
-		var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
-		if (dist > 1) {
-			dist = 1;
-		}
-		dist = Math.acos(dist);
-		dist = dist * 180/Math.PI;
-		dist = dist * 60 * 1.1515;
-		// if (unit=="K") { dist = dist * 1.609344 }
-    // if (unit=="N") { dist = dist * 0.8684 }
-    dist = dist * 1.609344
-		return Math.round(dist * 100) / 100;
+		// var radlat1 = Math.PI * lat1/180;
+		// var radlat2 = Math.PI * lat2/180;
+		// var theta = lon1-lon2;
+		// var radtheta = Math.PI * theta/180;
+		// var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+		// if (dist > 1) {
+		// 	dist = 1;
+		// }
+		// dist = Math.acos(dist);
+		// dist = dist * 180/Math.PI;
+		// dist = dist * 60 * 1.1515;
+		// // if (unit=="K") { dist = dist * 1.609344 }
+    // // if (unit=="N") { dist = dist * 0.8684 }
+    // dist = dist * 1.609344
+		// return Math.round(dist * 100) / 100;
 	}
 }
 
