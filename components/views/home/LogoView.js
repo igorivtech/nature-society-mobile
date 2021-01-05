4e8aec63ba8d05c98a26ae5d5ad0e60371e0dd58
Nimrod <nimrodbens@gmail.com>
2021-01-05 13:43:06 +0200

    good logo

--- components/views/home/LogoView.js
+++ components/views/home/LogoView.js
@@ -5,7 +5,7 @@ import { CARD_TRANSLATE_Y, ITEM_HEIGHT, SPACER_ITEM_SIZE } from "./PlaceCard";
 export const LogoView = memo(() => {
   return (
     <Animated.Image
-        
+        pointerEvents='none'
         source={require("../../../assets/images/hala_logo.png")}
         style={{
           position: 'absolute',
