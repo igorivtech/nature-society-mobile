f6f5b6902e9596654ec312992e3b8f56b626bc90
Nimrod <nimrodbens@gmail.com>
2021-01-06 21:01:56 +0200

    adding deep link

--- hooks/useDeepLink.js
+++ hooks/useDeepLink.js
@@ -3,12 +3,7 @@ import * as Linking from "expo-linking";
 import { SAVE_DEEP_LINK_ID } from "../context/userReducer";
 
 export const useDeepLink = (state, dispatch) => {
-
-  const handleUrl = ({ url }) => {
-    if (url == null) {
-      return;
-    }
-    const { queryParams } = Linking.parse(url);
+  const handleQueryParams = (queryParams) => {
     if (queryParams != null && queryParams.id != null) {
       dispatch({
         type: SAVE_DEEP_LINK_ID,
@@ -17,7 +12,17 @@ export const useDeepLink = (state, dispatch) => {
     }
   };
 
+  const handleUrl = ({ url }) => {
+    const { queryParams } = Linking.parse(url);
+    handleQueryParams(queryParams);
+  };
+
   useEffect(() => {
+    Linking.parseInitialURLAsync().then((url) => {
+      if (url != null && url.queryParams != null) {
+        handleQueryParams(url.queryParams);
+      }
+    });
     Linking.addEventListener("url", handleUrl);
     return () => {
       Linking.removeEventListener("url", handleUrl);
