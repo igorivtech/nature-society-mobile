ff84aeaa3153be9621b428d48787be8ed505190a
Nimrod <nimrodbens@gmail.com>
2021-01-06 20:56:16 +0200

    working on deep link

--- hooks/useDeepLink.js
+++ hooks/useDeepLink.js
@@ -1,11 +1,22 @@
 import { useEffect } from "react";
-import * as Linking from 'expo-linking';
+import * as Linking from "expo-linking";
+import { SAVE_DEEP_LINK_ID } from "../context/userReducer";
 
 export const useDeepLink = (state, dispatch) => {
+  const handleUrl = ({ url }) => {
+    const { queryParams } = Linking.parse(url);
+    if (queryParams != null && queryParams.id != null) {
+      dispatch({
+        type: SAVE_DEEP_LINK_ID,
+        payload: queryParams.id,
+      });
+    }
+  };
 
-
-  useEffect(()=>{
-
-  }, [])
-
+  useEffect(() => {
+    Linking.addEventListener("url", handleUrl);
+    return () => {
+      Linking.removeEventListener("url", handleUrl);
+    };
+  }, []);
 };
