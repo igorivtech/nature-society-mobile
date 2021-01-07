2eb58fe8fa87a9bc2ba927413e24d93cc9b4dacd
Nimrod <nimrodbens@gmail.com>
2021-01-07 10:52:28 +0200

    added location to deep link

--- hooks/useDeepLink.js
+++ hooks/useDeepLink.js
@@ -3,17 +3,23 @@ import * as Linking from "expo-linking";
 import { SAVE_DEEP_LINK_ID } from "../context/userReducer";
 import { useServer } from "./useServer";
 import { convertServerPlaces } from "./helpers";
+import * as Location from 'expo-location';
 
 export const useDeepLink = (dispatch) => {
   const { getPlace } = useServer();
 
   const handleQueryParams = (queryParams) => {
     if (queryParams != null && queryParams.id != null) {
-      getPlace(queryParams.id).then((place) => {
+      Promise.all([
+        Location.getLastKnownPositionAsync({}),
+        getPlace(queryParams.id)
+      ]).then((result) => {
+        const location = result[0];
+        const place = result[1];
         if (place != null) {
           dispatch({
             type: SAVE_DEEP_LINK_ID,
-            payload: convertServerPlaces([place])[0],
+            payload: convertServerPlaces([place], location?.coords)[0],
           });
         }
       });
