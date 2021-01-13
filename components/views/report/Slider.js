5d5517091b17dc5c34546c6bbd2d4012c74656d7
Nimrod <nimrodbens@gmail.com>
2021-01-13 17:18:17 +0200

    shorter wait time

--- components/views/report/Slider.js
+++ components/views/report/Slider.js
@@ -330,7 +330,7 @@ export const Slider = memo(({loaded, autoPlay, valueRef, item, location, showLoc
           setTimeout(() => {
             backToNormal();
           }, 1000);
-        }, 3000);
+        }, 2000);
       });
       // animation opacity? - yes, probably animation opacity.
       Animated.timing(animationOpacity, {
