6bab2c0e8113faded06de810921024abb4aa9ee2
Nimrod <nimrodbens@gmail.com>
2021-01-13 18:53:55 +0200

    fixed bug

--- components/screens/HomeScreen.js
+++ components/screens/HomeScreen.js
@@ -358,13 +358,14 @@ export const HomeScreen = ({ navigation, route }) => {
           }, 1000);
         }, 200);
       } else if (params.reportNow != null) {
+        const l = {...params.reportNow};
         navigation.setParams({reportNow: null});
         setListOpacity(0);
         setTimeout(() => {
           setListOpacity(0);
           setHideButtons(true);
           setHideList(true);  
-          navigation.navigate("Report", { location: {...params.reportNow}, currentPosition: locationRef.current });
+          navigation.navigate("Report", { location: l, currentPosition: locationRef.current });
           setTimeout(() => {
             setListOpacity(1);
           }, 1000);
