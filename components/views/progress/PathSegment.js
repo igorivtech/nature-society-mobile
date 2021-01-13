fcb4bc8ee3f16e14d1157b37491e0437746b49a7
Nimrod <nimrodbens@gmail.com>
2021-01-13 17:43:04 +0200

    added comment

--- components/views/progress/PathSegment.js
+++ components/views/progress/PathSegment.js
@@ -78,6 +78,7 @@ export const PathSegment = (({ pathHeight, currentIndex, scrollY, index, item, p
       if (!item.bottomDone) {
         // const p = user !== null ? user.numOfReports : 0;
         // setUserProgress(0.33 * (bottomImageObject.points - p)/bottomImageObject.points);
+        // 0.05 - 0.1
         setUserProgress(0.1);
       } else if (!item.topDone) {
         const userCurrentPoints = user !== null ? user.numOfReports : 0;
