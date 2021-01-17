af17d8b62d8ee04f1cb8c7d1d98c6c3752d375bb
Nimrod <nimrodbens@gmail.com>
2021-01-18 01:47:30 +0200

    refactor deep link

--- hooks/useDeepLink.js
+++ hooks/useDeepLink.js
@@ -3,54 +3,52 @@ import * as Linking from "expo-linking";
 import { SAVE_DEEP_LINK_ID } from "../context/userReducer";
 import { useServer } from "./useServer";
 import { convertServerPlaces } from "./helpers";
-import * as Location from 'expo-location';
+import * as Location from "expo-location";
 import { isStandalone } from "../values/consts";
 
 export const useDeepLink = (dispatch) => {
   const { getPlace } = useServer();
 
-  const handleQueryParams = (queryParams) => {
-    if (queryParams != null && queryParams.id != null) {
-      Promise.all([
-        Location.getLastKnownPositionAsync({}),
-        getPlace(queryParams.id)
-      ]).then((results) => {
-        const location = results[0];
-        const place = results[1];
-        if (place != null) {
-          dispatch({
-            type: SAVE_DEEP_LINK_ID,
-            payload: convertServerPlaces([place], location?.coords)[0],
-          });
-        }
-      });
-    }
+  const handlePlaceId = (placeId) => {
+    Promise.all([
+      Location.getLastKnownPositionAsync({}),
+      getPlace(placeId),
+    ]).then((results) => {
+      const location = results[0];
+      const place = results[1];
+      if (place != null) {
+        dispatch({
+          type: SAVE_DEEP_LINK_ID,
+          payload: convertServerPlaces([place], location?.coords)[0],
+        });
+      }
+    });
   };
 
   const handleUrl = ({ url }) => {
     if (url == null) {
       return;
     }
-    const idInArray = Linking.parse(url).path.split('/showPlace/').filter(e=>e != '');
+    const idInArray = Linking.parse(url)
+      .path.split("/showPlace/")
+      .filter((e) => e != "");
     if (idInArray.length === 1) {
-      handleQueryParams({
-        id: idInArray[0]
-      });
+      handlePlaceId(idInArray[0]);
     }
   };
 
   useEffect(() => {
     // if (isStandalone) {
     // } else {
-      Linking.parseInitialURLAsync().then((url) => {
-        if (url != null && url.path != null) {
-          handleUrl({url: url.path});
-        }
-      });
-      Linking.addEventListener("url", handleUrl);
-      return () => {
-        Linking.removeEventListener("url", handleUrl);
-      };
+    Linking.parseInitialURLAsync().then((url) => {
+      if (url != null && url.path != null) {
+        handleUrl({ url: url.path });
+      }
+    });
+    Linking.addEventListener("url", handleUrl);
+    return () => {
+      Linking.removeEventListener("url", handleUrl);
+    };
     // }
   }, []);
 };
