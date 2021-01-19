89bc2f7f93e941393daa125716ddd3b577d2a649
Nimrod <nimrodbens@gmail.com>
2021-01-19 13:42:09 +0200

    animations

--- components/views/progress/PathSegment.js
+++ components/views/progress/PathSegment.js
@@ -30,7 +30,7 @@ const BOTTOM_CONTAINER_WIDTH = 70;
 const BOTTOM_CONTAINER_MARGIN = 12;
 
 const MAX_SCALE = 0.7; // smallScreen ? 0.6 : 0.7
-const C = -0.4;
+const C = -0.45;
 
 export const PathSegment = (({ pathHeight, currentIndex, scrollY, index, item, popupVisible }) => {
 
