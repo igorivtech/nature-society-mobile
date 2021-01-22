4d87a9534168b8432c89a73f3167da897f947a73
Nimrod <nimrodbens@gmail.com>
2021-01-22 17:19:59 +0200

    added RTL hook

---
+++ hooks/useRTL.js
@@ -0,0 +1,27 @@
+import { useState, useEffect } from "react";
+import { I18nManager } from "react-native";
+import * as Updates from "expo-updates";
+
+export const useRTL = () => {
+  const [loadingRTL, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (I18nManager.isRTL) {
+      try {
+        I18nManager.forceRTL(false);
+      } catch (e) {console.error(e)}
+      try {
+        I18nManager.allowRTL(false);
+      } catch (e) {console.error(e)}
+      try {
+        Updates.reloadAsync().finally(()=>{
+          setLoading(false);
+        });
+      } catch (e) {console.error(e)}
+    } else {
+      setLoading(false);
+    }
+  }, []);
+
+  return { loadingRTL };
+};
