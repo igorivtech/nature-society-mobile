8fd42b9d936f233c3727c981bfdd919d8510cdc9
Nimrod <nimrodbens@gmail.com>
2021-02-15 20:04:02 +0200

    anim

--- components/screens/ReportScreen.js
+++ components/screens/ReportScreen.js
@@ -48,7 +48,7 @@ const crowd = {
   animation: require('../../assets/animations/crowd.json'),
   bg: require("../../assets/images/crowd_bg.png"),
   introAnimation: require('../../assets/animations/crowd_intro.json'),
-  outputRange: [0, 0.21621621621, 0.56756756756, 0.78378378378, 1],
+  outputRange: [0, 0.21621621621, 0.55855855855, 0.78378378378, 1],
   resizeMode: 'cover',
   thumb: require("../../assets/images/slider_thumb_crowd.png"),
   thumbBg: require("../../assets/images/slider_thumb_crowd_bg.png"),
