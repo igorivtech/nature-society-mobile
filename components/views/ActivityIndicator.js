dd6f71d752d93c7cd1c3003d2701ace5ed89bd9d
Nimrod <nimrodbens@gmail.com>
2021-02-18 17:04:55 +0200

    lottie indicator

--- components/views/ActivityIndicator.js
+++ components/views/ActivityIndicator.js
@@ -1,6 +1,14 @@
-import React from 'react';
-import {ActivityIndicator as AI} from 'react-native';
+import React from "react";
+import LottieView from "lottie-react-native";
 
-export const ActivityIndicator = ({style = {}, animating = true, color}) => {
-    return <AI style={style} color={color} animating={animating} />
-}
+export const ActivityIndicator = ({ style = {}, animating = true, color }) => {
+  return (
+    <LottieView
+      style={[style, { height: 20, width: 20, opacity: animating ? 1 : 0 }]}
+      source={require("../../assets/animations/buy_spin.json")}
+      loop={true}
+      resizeMode='contain'
+      autoPlay={animating}
+    />
+  );
+};
