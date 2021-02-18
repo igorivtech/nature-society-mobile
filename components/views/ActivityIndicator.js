d0b782fb7badaec97926502d902a7d4e23d3d8d9
Nimrod <nimrodbens@gmail.com>
2021-02-18 17:09:21 +0200

    refactor

--- components/views/ActivityIndicator.js
+++ components/views/ActivityIndicator.js
@@ -8,15 +8,17 @@ export const ActivityIndicator = ({ style = {}, animating = true, color }) => {
       style={styles.lottie(style, animating)}
       source={require("../../assets/animations/buy_spin.json")}
       loop={true}
-      resizeMode='contain'
+      resizeMode="contain"
       autoPlay={animating}
     />
   );
 };
 
 const styles = StyleSheet.create({
-    lottie: (style, animating) => ({
-        ...style,
-        height: 20, width: 20, opacity: animating ? 1 : 0
-    })
-})
+  lottie: (style, animating) => ({
+    ...style,
+    height: 20,
+    width: 20,
+    opacity: animating ? 1 : 0,
+  }),
+});
