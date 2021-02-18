80e4e3f7d2ea9487de0cf6ab8e0cd1ed9e033c7f
Nimrod <nimrodbens@gmail.com>
2021-02-18 17:29:32 +0200

    refactor

--- components/views/ActivityIndicator.js
+++ components/views/ActivityIndicator.js
@@ -1,25 +1,40 @@
 import React from "react";
 import LottieView from "lottie-react-native";
-import { StyleSheet } from "react-native";
+import { Animated, Easing, StyleSheet } from "react-native";
+import { useRef } from "react";
+import { useEffect } from "react";
+import { memo } from "react";
 
-export const ActivityIndicator = ({style = {}, animating = true, color, customKey = ""}) => {
+export const ActivityIndicator = memo(({style = {}, animating = true, color, customKey = ""}) => {
+  const opacity = useRef(new Animated.Value(animating ? 1 : 0)).current;
+  useEffect(()=>{
+    Animated.timing(opacity, {
+      toValue: animating ? 1 : 0,
+      useNativeDriver: true,
+      easing: Easing.inOut(Easing.ease)
+    }).start();
+  }, [animating]);
   return (
-    <LottieView
-      key={customKey}
-      style={styles.lottie(style, animating)}
-      source={require("../../assets/animations/buy_spin.json")}
-      loop={true}
-      resizeMode="contain"
-      autoPlay={animating}
-    />
+    <Animated.View style={styles.container(opacity)}>
+      <LottieView
+        key={customKey}
+        style={styles.lottie(style)}
+        source={require("../../assets/animations/buy_spin.json")}
+        loop={true}
+        resizeMode="contain"
+        autoPlay={animating}
+      />
+    </Animated.View>
   );
-};
+});
 
 const styles = StyleSheet.create({
-  lottie: (style, animating) => ({
+  container: (opacity) => ({
+    opacity
+  }),
+  lottie: (style) => ({
     ...style,
     height: 20,
     width: 20,
-    opacity: animating ? 1 : 0,
   }),
 });
