1337c6f01099ef0e3347ad3061c04ae9fb105328
Nimrod <nimrodbens@gmail.com>
2021-03-02 19:43:42 +0200

    push token

--- hooks/useServer.js
+++ hooks/useServer.js
@@ -170,19 +170,18 @@ export const useServer = () => {
   }
 
   const updatePushToken = async (pushToken) => {
-    // try {
-    //   await fetch(`${BASE_URL}/updatePushToken`, {
-    //     method: "POST",
-    //     body: JSON.stringify({
-    //       pushToken
-    //     }),
-    //     headers: {
-    //       'Content-Type': 'application/json',
-    //       Authorization: 'guest'
-    //     }
-    //   });
-    //   AsyncStorage.setItem("LAST_SENT_TOKEN", pushToken).then(()=>{});
-    // } catch (error) {}
+    try {
+      await fetch(`${BASE_URL}/saveToken`, {
+        method: "POST",
+        body: JSON.stringify({
+          token: pushToken
+        }),
+        headers: {
+          'Content-Type': 'application/json',
+        }
+      });
+      AsyncStorage.setItem("LAST_SENT_TOKEN", pushToken).then(()=>{});
+    } catch (error) {}
   }
 
   const sendReport = async (token, reportData) => {
