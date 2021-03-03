a285371ed39fd64afa2a552c86a1c02bf9fc7178
Nimrod <nimrodbens@gmail.com>
2021-03-03 17:08:15 +0200

    more delay

--- components/screens/HomeScreen.js
+++ components/screens/HomeScreen.js
@@ -244,7 +244,7 @@ export const HomeScreen = ({ navigation, route }) => {
         setTimeout(() => {
           ignoreCardsListener.current = false;
           lockAutoSearching.current = false;
-        }, MAP_ANIMATION_DURATION+10);
+        }, MAP_ANIMATION_DURATION+100);
       }, SPLASH_HIDE_DELAY*0.6);
     } else {
       lockAutoSearching.current = false;
