123c1894be969fd61fc46a4eb442ce65048c3598
Nimrod <nimrodbens@gmail.com>
2021-03-03 18:44:32 +0200

    timeout

--- components/screens/HomeScreen.js
+++ components/screens/HomeScreen.js
@@ -244,7 +244,7 @@ export const HomeScreen = ({ navigation, route }) => {
         setTimeout(() => {
           ignoreCardsListener.current = false;
           lockAutoSearching.current = false;
-        }, MAP_ANIMATION_DURATION+200);
+        }, MAP_ANIMATION_DURATION+300);
       }, SPLASH_HIDE_DELAY*0.6);
     } else {
       lockAutoSearching.current = false;
